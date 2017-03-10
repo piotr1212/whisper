ff0ca7897c98dacdfdd9eef751d5278635c8ae2c
Piotr Popieluch <piotr1212@gmail.com>
2017-03-10 12:11:27 +0100

    restructure project and switch to setuptools
    Renamed the cli tools from bin/* and contrib/* to filenames without dashes so
    they are loadable and easier to unittest. Moved those files to a directory in
    the lib dir. Created a module subdirectory keep the pythonpath root dir clean.
    
    Switch to setuptools so that scripts can be provided with `console_scripts`.

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 
 import os
-from glob import glob
-from distutils.core import setup
+from setuptools import setup
 
 
 setup(
@@ -13,8 +12,26 @@ setup(
   author_email='chrismd@gmail.com',
   license='Apache Software License 2.0',
   description='Fixed size round-robin style database',
-  py_modules=['whisper'],
-  scripts=glob('bin/*') + glob('contrib/*'),
+  entry_points={
+    'console_scripts': [
+      'find-corrupt-whisper-files=whisper.cli.findcorruptwhisperfiles:main',
+      'rrd2whisper=whisper.cli.rrd2whisper:main',
+      'whisper-create=whisper.cli.create:main',
+      'whisper-diff=whisper.cli.diff:main',
+      'whisper-dump=whisper.cli.dump:main',
+      'whisper-fetch=whisper.cli.fetch:main',
+      'whisper-fill=whisper.cli.fill:main',
+      'whisper-info=whisper.cli.info:main',
+      'whisper-merge=whisper.cli.merge:main',
+      'whisper-resize=whisper.cli.resize:main',
+      'whisper-set-aggregation-method=whisper.cli.setaggregationmethod:main',
+      'whisper-set-xfilesfactor=whisper.cli.setxfilesfactor:main',
+      'whisper-update=whisper.cli.update:main',
+      'whisper-auto-resize=whisper.contrib.autoresize:main',
+      'whisper-auto-update=whisper.contrib.autoupdate:main',
+    ],
+  },
+  packages=['whisper', 'whisper.cli', 'whisper.contrib'],
   classifiers=[
     'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 2.6',
